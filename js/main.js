17abe7789705e669b0e0937dd296bb79c11f6738
Shadman Jahin <jahin.web@gmail.com>
2025-06-29 20:04:17 +0600

    navigation

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const listIcon = document.querySelector(".bi-list");
+const slider = document.querySelector("nav .content");
+const exit = slider.querySelector("i");
+console.log(exit);
+listIcon.addEventListener("click", () => {
+    slider.style.setProperty("right", "0");
+    slider.style.setProperty("transition", ".3s");
+});
+
+exit.addEventListener("click", () => {
+    slider.style.setProperty("right", "-250px");
+})
+
